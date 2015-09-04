533b799b7ecd64e721968227b075819afe65da1d
Ivan Scherbak <dev@funivan.com>
2015-09-04 09:56:30 +0300

    fix: #1 use custom settings

---
+++ thundersync@ducic.ac.in/defaults/preferences/prefs.js
@@ -0,0 +1,5 @@
+pref("extensions.thundersync.serverUrl", "http://erp:71/dev/bit/in.ac.ducic.tabs/getJSON");
+pref("extensions.thundersync.serverRequestMethod", "POST");
+pref("extensions.thundersync.serverSendData", "AUTH={'UserName':'Prashant','Token':'SOMETHING'}");
+pref("extensions.thundersync.addressBookName", "Reva Cranes Synchronized");
+pref("extensions.thundersync.responseSuccessStatus", 251);
